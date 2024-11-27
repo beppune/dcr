3c44762f9e11f5f35ba15cec498564c233d6f4e9
beppune <manzo.giuseppe@gmail.com>
2024-11-27 19:23:17 +0100

    iterate over table

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use ureq;
 use std::fs::File;
 use std::io::Write;
-use std::io::Read;
+
+use scraper::Html;
+use scraper::Selector;
 
 fn main() -> Result<(), ureq::Error> {
 
@@ -12,11 +14,26 @@ fn main() -> Result<(), ureq::Error> {
 		.set("Content-Type", "application/x-www-form-urlencoded")
         .send(body);
 
-    if let Ok(resp) = res {
-        println!("{}", resp.status_text());
+    if let Err(err) = res {
+		println!("{:?}", err);
+		std::process::exit(1);
+    }
+	
+	let resp = res.unwrap();
+	println!("{}", resp.status_text());
 
-        let mut f = File::create("resp.html").unwrap();
-        f.write( resp.into_string()?.as_bytes() ).unwrap();
+	let mut f = File::create("resp.html").unwrap();
+    let content = resp.into_string()?;
+	f.write( content.as_bytes() ).unwrap();
+	
+	let table_selector = Selector::parse("#ADC_ContenutoSpecificoPagina_gvGiornaliero > tbody tr").unwrap();
+    
+    let doc = Html::parse_document(&content);
+
+    let rows = doc.select(&table_selector);
+
+    for tr in rows {
+        println!("{}", tr.inner_html());
     }
 
     Ok(())    
