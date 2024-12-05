765784e74c834fd888a7a6a39ef916a677c427e9
beppune <manzo.giuseppe@gmail.com>
2024-12-05 12:54:31 +0100

    enter uname and password

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,27 @@ fn main() -> Result<(), ureq::Error> {
 
     let mut username = String::new();
     print!("Username: ");
+    let _ = std::io::stdout().flush();
     std::io::stdin()
         .read_line(&mut username)
         .unwrap();
 
     let password = rpassword::prompt_password("Password: ")
         .unwrap();
-    
-    get_report()
+
+    get_report(username.to_string().trim(), &password)
 }
 
-fn get_report() -> Result<(), ureq::Error> {
+fn get_report(username:&str, password:&str) -> Result<(), ureq::Error> {
+
+    let mut basic_auth = format!( "rete\\{}:{}", username, password );
+    basic_auth = format!("Basic {}", simple_base64::encode(basic_auth) );
+    dbg!(&basic_auth);
 
     let body = File::open("body.txt").unwrap();
 
     let res = ureq::post("http://10.194.137.36/ACCESSIDC/ReportGiornaliero.aspx")
-        .set("Authorization", "Basic cmV0ZVxtYW56b2dpOTo0S3J1bTFyMQ==")
+        .set("Authorization", &basic_auth )
         .set("Content-Type", "application/x-www-form-urlencoded")
         .set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
         .set("Accept-Language", "it-IT,it;q=0.9")
